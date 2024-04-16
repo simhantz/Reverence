4b8b9cf9570e6d291c602a91cf21d5bcbc07384b
schnitzel <150812068+simhantz@users.noreply.github.com>
2024-04-16 19:02:10 +0200

    Smoother cam follow, again....

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    private Vector3 offset = new Vector3(0f, 0, -10f);
+    private Vector3 velocity = Vector3.zero;
     [Header("Refrences")]
-    public Transform Player;
+    public Transform player;
 
     [Header("Adjustments")]
     public float cameraHeight;
+    public float smoothTime = 0.25f;
+    private void Start()
+    {
+        offset = new Vector3(offset.x, cameraHeight,offset.z);
+    }
 
     void Update()
     {
-        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + cameraHeight, transform.position.z);
+        Vector3 offsetPosition = player.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, offsetPosition, ref velocity, smoothTime);
     }
 }
--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -7,19 +7,33 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Transform groundCheck;
 
+    private float _speed;
+    private float lastDirection;
     private float direction;
+    private float groundCheckRadius = 0.2f;
     private int airJumpCount = 0;
 
+    [Header("Refrences")]
     public LayerMask groundLayer;
 
-    public int maxJumpCount = 2;
+    [Header("Move Speeds")]
     public float speed = 10f;
+    public float sprintSpeed = 20f;
+    public float dashPower = 20f;
+
+    [Header("Jump Settings")]
+    public int maxJumpCount = 2;
     public float jumpPower = 16f;
     public float airJumpPower = 10f;
-    public float groundCheckRadius = 0.2f;
+
+    [Header("Keybinds")]
+    public KeyCode sprintKey =KeyCode.LeftControl;
+    public KeyCode dashKey = KeyCode.LeftShift;
+
 
     void Start()
     {
+        _speed = speed;
         rb = GetComponent<Rigidbody2D>();
         groundCheck = gameObject.transform.GetChild(0);
     }
@@ -27,11 +41,18 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        if (direction != 0)
+        {
+            lastDirection = direction;
+        }
         direction = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(speed * direction, rb.velocity.y);
 
-        
-        Jump(isGrounded());
+        bool isGrounded = IsGrounded();
+
+        Sprint(isGrounded);
+        Jump(isGrounded);
+        Dash(isGrounded, direction, lastDirection);
     }
 
     void Jump(bool isGrounded)
@@ -40,7 +61,7 @@ public class PlayerController : MonoBehaviour
         {
             if (Input.GetButtonUp("Jump") && rb.velocity.y >= 0f)
             {
-                rb.velocity *= 0.5f;
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
             }
             if (Input.GetButtonDown("Jump") && airJumpCount < maxJumpCount)
             {
@@ -60,8 +81,32 @@ public class PlayerController : MonoBehaviour
         }
         
     }
-
-    bool isGrounded()
+    void Sprint(bool isGrounded)
+    {
+        if (!isGrounded)
+        {
+            speed = _speed;
+            return;
+        }
+        if (Input.GetKey(sprintKey))
+        {
+            speed = sprintSpeed;
+        }
+        else speed = _speed;
+    }
+    // Dash? More like "blink" am I right? Ha Ha Ha!!!
+    void Dash(bool isGrounded, float dir, float lastDir)
+    {
+        if (Input.GetKeyDown(dashKey) && dir != 0)
+        {
+            rb.velocity = new Vector2(dashPower * direction, rb.velocity.y);
+        }
+        else if (Input.GetKeyDown(dashKey))
+        {
+            rb.velocity = new Vector2(dashPower * lastDir, rb.velocity.y);
+        }
+    }
+    bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
