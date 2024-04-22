5d2684012014b2454fc4a54e564409d6f12de522
22hansim(Skola) <22hansim@soltorgsgymnasiet.se>
2024-04-22 11:57:48 +0200

    Nothing

--- Assets/Scripts/Interact Scripts/BetterInteract.cs
+++ Assets/Scripts/Interact Scripts/BetterInteract.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BetterInteract : MonoBehaviour
+public abstract class BetterInteract : MonoBehaviour
 {
     [SerializeField] private float interactRange = 1f;
     void Update()
     {
+        
+    }
+    public virtual void Interact()
+    {
         Collider2D collider = Physics2D.OverlapCircle(transform.position, interactRange);
-        //if (collider.TryGetComponent<>)
-        //{
+        if (collider.TryGetComponent<InteractCatch>(out InteractCatch interactCatch))
+        {
 
-        //}
+        }
     }
 }
---
+++ Assets/Scripts/Interact Scripts/InteractCatch.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractCatch : BetterInteract
+{
+    // Refrence to different class
+    public override void Interact()
+    {
+        base.Interact();
+
+    }
+}
